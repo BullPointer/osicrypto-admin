0302ddbc608c084e2b8f5514d75a3f48a78960e9
White Bull <blueded88@gmail.com>
2023-09-26 12:02:53 -0400

    made the support.tsx more responsive

--- src/components/Documents/Support.tsx
+++ src/components/Documents/Support.tsx
@@ -47,16 +47,16 @@ const Support = () => {
         <div className="flex flex-row justify-between items-start py-3 px-2 bg-[#fff] shadow-sm shadow-black">
           <div className="flex flex-row justify-center items-center gap-2">
             <Icon
-              className="text-[#9b7e7e] font-bold text-[25px]"
+              className="text-[#9b7e7e] font-bold text-[21px] sm:text-[25px]"
               icon="material-symbols:contact-support-outline"
             />
-            <div className="text-[#666363] font-bold text-[18px]">
+            <div className="text-[#666363] font-bold text-[12px] sm:text-[18px]">
               Support Requests
             </div>
           </div>
         </div>
         <div className="pt-5 pb-10 my-4 bg-[#fff] rounded-md">
-          <div className="w-full py-2 px-4 font-bold text-[15px] bg-[#fff] shadow-md shadow-black">
+          <div className="w-full py-2 px-4 font-bold text-[11px] sm:text-[15px] bg-[#fff] shadow-md shadow-black">
             List of Support Requests
           </div>
           <div className="w-full py-2 px-4 font-bold text-[15px] bg-[#fff] shadow-md shadow-black">
@@ -70,94 +70,109 @@ const Support = () => {
               />
             </div>
           </div>
-          <div className="grid grid-cols-8 w-full py-2 px-4 font-bold text-[15px] bg-[#fff] shadow-md shadow-black">
-            {[
-              "Ticket ID",
-              "Status",
-              "Category",
-              "Subject",
-              "Priority",
-              "Created on",
-              "Last Updated on",
-              "Actions",
-            ].map((list, index) => (
-              <div className="text-[#444] font-serif text-[14px]" key={index}>
-                {list}
-              </div>
-            ))}
-          </div>
-          {isLoading ? (
-            <div className="py-10 w-full flex flex-col justify-center items-center bg-[#fff] shadow-md shadow-black">
-              <Icon className="text-[100px]" icon="eos-icons:bubble-loading" />
-            </div>
-          ) : support.length > 0 ? (
-            <div>
-              {support?.map(
-                (
-                  {
-                    _id,
-                    category,
-                    status,
-                    subject,
-                    priority,
-                    createdDate,
-                    updatedDate,
-                  },
-                  index
-                ) => (
+          <div className="overflow-x-auto bg-[#fff] shadow-md shadow-black">
+            <div className="min-w-[1000px] lg:w-full">
+              <div className="grid grid-cols-8 w-full py-2 px-4 font-bold text-[15px]">
+                {[
+                  "Ticket ID",
+                  "Status",
+                  "Category",
+                  "Subject",
+                  "Priority",
+                  "Created on",
+                  "Last Updated on",
+                  "Actions",
+                ].map((list, index) => (
                   <div
+                    className="text-[#444] font-serif text-[14px]"
                     key={index}
-                    className="grid grid-cols-8 w-full py-2 px-4 font-bold text-[15px] bg-[#fff] shadow-md shadow-black"
                   >
-                    <div className="z-10 break-words text-[10px] md:text-[12px] pr-2">
-                      {_id}
-                    </div>
-                    <div className="word-wrap text-[10px] md:text-[12px]">
-                      {status}
-                    </div>
-                    <div className="word-wrap text-[10px] md:text-[12px]">
-                      {category}
-                    </div>
-                    <div className="word-wrap text-[10px] md:text-[12px]">
-                      {subject}
-                    </div>
-                    <div className="word-wrap text-[10px] md:text-[12px]">
-                      {priority}
-                    </div>
-                    <div className="break-words pr-2 text-[10px] md:text-[12px]">
-                      {createdDate}
-                    </div>
-                    <div className="word-wrap text-[10px] md:text-[12px]">
-                      {updatedDate}
-                    </div>
-                    <div className="flex flex-row justify-start items-center gap-1">
-                      <Link to={`${_id}`}>
-                        <Icon
-                          className="text-[14px] md:text-[18px]"
-                          icon="tabler:edit"
-                        />
-                      </Link>
-                      <div
-                        onClick={() =>
-                          setConfirm({ ...confirm, id: _id, confirm: true })
-                        }
-                      >
-                        <Icon
-                          className="text-[14px] md:text-[18px] cursor-pointer"
-                          icon="material-symbols:delete-outline"
-                        />
-                      </div>
-                    </div>
+                    {list}
                   </div>
-                )
+                ))}
+              </div>
+              {isLoading ? (
+                <div className="py-10 w-full flex flex-col justify-center items-center bg-[#fff] shadow-md shadow-black">
+                  <Icon
+                    className="text-[100px]"
+                    icon="eos-icons:bubble-loading"
+                  />
+                </div>
+              ) : support.length > 0 ? (
+                <div>
+                  {support?.map(
+                    (
+                      {
+                        _id,
+                        category,
+                        status,
+                        subject,
+                        priority,
+                        createdDate,
+                        updatedDate,
+                      },
+                      index
+                    ) => (
+                      <div
+                        key={index}
+                        className="grid grid-cols-8 w-full py-2 px-4 font-bold text-[15px] bg-[#fff] shadow-md shadow-black"
+                      >
+                        <div className="z-10 break-words text-[10px] md:text-[12px] pr-2">
+                          {_id}
+                        </div>
+                        <div className="word-wrap text-[10px] md:text-[12px]">
+                          {status}
+                        </div>
+                        <div className="word-wrap text-[10px] md:text-[12px]">
+                          {category}
+                        </div>
+                        <div className="word-wrap text-[10px] md:text-[12px]">
+                          {subject}
+                        </div>
+                        <div className="word-wrap text-[10px] md:text-[12px]">
+                          {priority}
+                        </div>
+                        <div className="break-words pr-2 text-[10px] md:text-[12px]">
+                          {createdDate}
+                        </div>
+                        <div className="word-wrap text-[10px] md:text-[12px]">
+                          {updatedDate}
+                        </div>
+                        <div className="flex flex-row justify-start items-center gap-1">
+                          <Link to={`${_id}`}>
+                            <Icon
+                              className="text-[14px] md:text-[18px]"
+                              icon="tabler:edit"
+                            />
+                          </Link>
+                          <div
+                            onClick={() =>
+                              setConfirm({ ...confirm, id: _id, confirm: true })
+                            }
+                          >
+                            <Icon
+                              className="text-[14px] md:text-[18px] cursor-pointer"
+                              icon="material-symbols:delete-outline"
+                            />
+                          </div>
+                        </div>
+                      </div>
+                    )
+                  )}
+                </div>
+              ) : (
+                <div className="py-10 w-full flex flex-col justify-center items-center bg-[#fff] shadow-md shadow-black">
+                  <Icon
+                    className="text-[100px]"
+                    icon="openmoji:office-worker"
+                  />
+                  <div className="text-[13px]">
+                    You have no support request yet
+                  </div>
+                </div>
               )}
             </div>
-          ) : (
-            <div className="py-10 w-full flex flex-col justify-center items-center bg-[#fff] shadow-md shadow-black">
-              <Icon className="text-[100px]" icon="openmoji:office-worker" />
-              <div className="text-[13px]">You have no support request yet</div>
-            </div>
-          )}
+          </div>
         </div>
       </div>
     </>
